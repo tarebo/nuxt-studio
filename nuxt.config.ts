8399c79f5f91ac831a74bceefe6c60525d8d9438
fl_kishimoto <kishimoto@framelunch.jp>
2025-07-08 20:56:49 +0900

    syling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,29 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
   ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=Noto+Sans+JP:wght@100..900&family=Noto+Serif+JP:wght@200..900&family=WDXL+Lubrifont+JP+N&display=swap'
+        }
+      ]
+    }
+  },
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
   compatibilityDate: '2024-04-03',
+  css: ['./assets/css/main.css']
 })
